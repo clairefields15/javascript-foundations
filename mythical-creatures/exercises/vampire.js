566fb33f75bb8ac01c3cc95081bbf6d9a4e68be1
Claire Fields <clairefields15@gmail.com>
2021-03-29 20:10:23 -0600

    pass all vampire tests

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,23 @@
+class Vampire {
+  constructor(name, pet){
+    this.name = name;
+
+    this.pet = pet;
+      if (pet === undefined){
+        this.pet = 'bat'
+        }
+
+    this.thirsty = true
+    this.ouncesDrank = 0
+  }
+//might be a more elegant way to pass the I'm too full test
+  drink(){
+    this.thirsty = false
+    if (this.ouncesDrank <= 40){
+      this.ouncesDrank += 10
+    }
+    return 'I\'m too full to drink anymore!'
+  }
+}
+
+module.exports = Vampire

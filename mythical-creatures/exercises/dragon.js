83f72a6e740c24fb95a339e9e8fda7c226a7fe5a
Claire Fields <clairefields15@gmail.com>
2021-03-29 19:49:00 -0600

    pass dragon all dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,24 @@
+var eatCounter = 0
 
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  }
+
+  greet(){
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    eatCounter++
+    if (eatCounter >= 3){
+      return this.hungry = false
+    }
+  }
+}
+
+
+module.exports =
+  Dragon

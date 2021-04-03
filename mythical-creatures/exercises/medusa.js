1249e17b62c7cc642d6fc9dea2aef5286ae4e88d
Claire Fields <clairefields15@gmail.com>
2021-04-03 15:02:32 -0600

    pass medusa tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,25 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name){
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim) {
+    var soonToBeStatue = new Statue (victim.name)
+    if (this.statues.length <= 2) {
+      this.statues.push(soonToBeStatue)
+    } else if (this.statues.length >= 3){
+      var releasedPerson = new Person (this.statues[0].name)
+      releasedPerson.mood = 'relieved'
+      this.statues.shift();
+      this.statues.push(soonToBeStatue);
+      return releasedPerson
+    }
+  }
+}
+
+
+module.exports = Medusa;

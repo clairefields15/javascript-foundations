72eac2d3796c9b43a9e0a3a5dfba2f7fc910354d
Claire Fields <clairefields15@gmail.com>
2021-04-07 08:56:41 -0600

    do you want to build a snowman

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,6 +1,31 @@
 var Snowman = require('./Snowman');
 
 class Human {
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {snowballs: 0, coal: 0, buttons: 0, carrots: 0};
+  }
+
+
+  gatherMaterials(item, num) {
+    this.materials[item]+= num
+
+
+    // if(item === 'carrots') {
+    //   this.materials.carrots += num
+    // } else if (item === 'buttons'){
+    //   this.materials.buttons += num
+    // } else if (item === 'coal'){
+    //   this.materials.coal += num
+    // }
+  }
+
+  buildASnowman() {
+    var newSnowman = new Snowman({coal: this.materials.coal,carrots: this.materials.carrots,snowballs: this.materials.snowballs})
+    return newSnowman
+    console.log(newSnowman)
+  }
 
 }
 
